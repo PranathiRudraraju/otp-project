b9000100fa7266d92934caa71ff1feeae193b304
PranathiRudraraju <rudrarajupranathi25@gmail.com>
2025-11-21 19:43:57 +0530

    Fix OTP POST request issue

--- app/views.py
+++ app/views.py
@@ -2,30 +2,39 @@ import random
 from django.shortcuts import redirect, render
 from django.core.mail import send_mail
 
-import OTP
 
-# Create your views here.
 def login_view(request):
-    email=request.POST.get('email')
-    otp=str(random.randint(1000,9999))
-    request.session['email']=email
-    request.session['otp']=otp
-    send_mail(
-        'your otp code',
-        f'Your otp is:{otp}',
-        'pranathirudraraju80@gmail.com',
-        [email],
-    )
-    if request.method=='POST':
+    if request.method == 'POST':
+        email = request.POST.get('email')
+
+        # Generate OTP
+        otp = str(random.randint(1000, 9999))
+
+        # Save to session
+        request.session['email'] = email
+        request.session['otp'] = otp
+
+        # Send OTP email
+        send_mail(
+            'Your OTP Code',
+            f'Your OTP is: {otp}',
+            'pranathirudraraju80@gmail.com',
+            [email],
+        )
+
         return redirect('verify_otp')
-    return render(request,'app/login.html')
+
+    return render(request, 'app/login.html')
+
 
 def verify_otp(request):
-    if  request.method=='POST':
-        user_otp=request.POST.get('user_otp')
-        session_otp=request.session.get('otp')
+    if request.method == 'POST':
+        user_otp = request.POST.get('user_otp')
+        session_otp = request.session.get('otp')
+
         if user_otp == session_otp:
-            return render(request,'app/success.html')
+            return render(request, 'app/success.html')
         else:
-            return render(request,'app/failure.html')
-    return render(request,'app/verify_otp.html')
+            return render(request, 'app/failure.html')
+
+    return render(request, 'app/verify_otp.html')
