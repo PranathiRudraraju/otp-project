57bce7aa74f29fa26b268ffcdfaf8cdcb13063d2
PranathiRudraraju <rudrarajupranathi25@gmail.com>
2025-11-21 19:59:00 +0530

    Fix email backend

--- OTP/settings.py
+++ OTP/settings.py
@@ -121,12 +121,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'pranathirudraraju80@gmail.com'
-EMAIL_HOST_PASSWORD = 'xhvsjrbtqoqktfhg' # Use App Password if Gmail
+EMAIL_HOST_PASSWORD = 'xhvsjrbtqoqktfhg'  # Your Gmail App Password
 EMAIL_USE_TLS = True
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
 
